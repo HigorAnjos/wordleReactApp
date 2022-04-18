61cd3587a446514f0b2b93c0f4e8f1ce52dc94f5
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-04-18 17:57:06 -0300

    build

---
+++ build/precache-manifest.be221aa58accc0e0edb3fd0b3849c53f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf34dedc9a2677ac7bd8f2d6de2cf2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c283613cc7ebb806e62b",
+    "url": "/static/css/main.bb3dcffa.chunk.css"
+  },
+  {
+    "revision": "9607b35b2570bb6a56d7",
+    "url": "/static/js/2.559a4a0d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.559a4a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c283613cc7ebb806e62b",
+    "url": "/static/js/main.348141d0.chunk.js"
+  },
+  {
+    "revision": "9c674a068e8b7a268b2c",
+    "url": "/static/js/runtime-main.ea46ed7d.js"
+  },
+  {
+    "revision": "44a432e2857f632df811dca836916c74",
+    "url": "/static/media/index.44a432e2.txt"
+  }
+]);
